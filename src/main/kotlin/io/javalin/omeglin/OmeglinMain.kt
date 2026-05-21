2d057eb639926c23aacd592df0c1bd8dd538229c
Krzysztof Szewczyk <k.szewczyk@kainos.com>
2026-05-22 00:24:11 +0200

    Enable self-sign cert

--- src/main/kotlin/io/javalin/omeglin/OmeglinMain.kt
+++ src/main/kotlin/io/javalin/omeglin/OmeglinMain.kt
@@ -2,10 +2,34 @@ package io.javalin.omeglin
 
 import io.javalin.Javalin
 import io.javalin.http.staticfiles.Location
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
+import org.eclipse.jetty.util.ssl.SslContextFactory
 
 fun main() {
-    Javalin.create {
-        it.staticFiles.add("src/main/resources/public", Location.EXTERNAL)
-        it.routes.ws("/api/matchmaking", Matchmaker::websocket)
-    }.start("0.0.0.0", 7070)
+    val keystorePath = System.getProperty("keystore.path", "keystore.jks")
+    val keystorePass = System.getProperty("keystore.pass", "changeit")
+    val useHttps     = java.io.File(keystorePath).exists()
+
+    Javalin.create { config ->
+        config.staticFiles.add("src/main/resources/public", Location.EXTERNAL)
+        config.routes.ws("/api/matchmaking", Matchmaker::websocket)
+        if (useHttps) {
+            config.jetty.modifyServer { server ->
+                server.connectors.forEach { it.stop(); it.destroy() }
+                server.connectors = emptyArray()
+                val ssl = SslContextFactory.Server()
+                ssl.keyStorePath     = keystorePath
+                ssl.keyStorePassword = keystorePass
+                ssl.keyManagerPassword = keystorePass
+                val https = ServerConnector(server, ssl)
+                https.host = "0.0.0.0"
+                https.port = 7070
+                server.addConnector(https)
+            }
+        }
+    }.start("0.0.0.0", if (useHttps) 0 else 7070).also {
+        val scheme = if (useHttps) "https" else "http"
+        println("FreestyleArena running at $scheme://0.0.0.0:7070")
+    }
 }
